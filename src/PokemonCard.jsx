a0cb1f938b3f485b9a18d8c725c5196be1d8bf43
RoninMirrorImage <spina.amedeo.pio@gmail.com>
2023-02-10 13:17:12 +0100

    Sistemare problema key in evolution

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -17,71 +17,77 @@ const PokemonCard = ({pkData}) => {
 
     return (
         <div className="pokemon-card">
-            <div className="identifier-section">
-                <div className="name-field">
-                    <p className="identifier-name">{firstLetterToUpperCaseFormatFunc(pkData.name)}</p>
-                </div>
-
-                <div className="identification-number-field">
-                    <p className="identifier-number">{`N°${pkData.id}`}</p>
-                </div>
-            </div>
-
-            <div className="pokemon-image">
-                <img src={pokemonGenderImageSelect()} alt="PokemonImage" />
-            </div>
-
-            <div className="characteristics">
-
-                <div className="height-field">
-                    <p className="field-name">
-                        Height
-                    </p>
-                    <p>
-                        {pkData.height/10 + " m"}
-                    </p>
-                </div>
-
-                <div className="weight-field">
-                    <p className="field-name">
-                        Weight
-                    </p>
-                    <p>
-                        {pkData.weight/10 + " kg"}
-                    </p>
-                </div>
-
-                <div className="skills-field">
-                    <p className="field-name">
-                        Skills
-                    </p>
-                    <ul>
-                        <PokemonSkillsField pkData={pkData}/>
-                    </ul>
-                </div>
-
-                <div className="gender-field">
-                    <p className="field-name">
-                        Gender
-                    </p>
-                    <div className="gender-icons">
-                        <PokemonGenderField pkData={pkData} />
+            <div className="poke-presentation">
+                <div className="identifier-section">
+                    <div className="name-field">
+                        <p className="identifier-name">{firstLetterToUpperCaseFormatFunc(pkData.name)}</p>
                     </div>
-                </div>       
+
+                    <div className="identification-number-field">
+                        <p className="identifier-number">{`N°${pkData.id}`}</p>
+                    </div>
+                </div>
+
+                <div className="pokemon-image">
+                    <img src={pokemonGenderImageSelect()} alt="PokemonImage" />
+                </div>
             </div>
 
-            <div className="type-field">
-                <p>Type</p>
-                <ul>
-                    <PokemonTypeField pkData={pkData}/>
-                </ul>
-            </div>
+            <div className="poke-info">
+                <div className="characteristics">
 
-            <div className="weaknesses-field">
-                <p>Weaknesses</p>
-                <ul>
-                    <PokemonWeaknessesField pkData={pkData}/>
-                </ul>
+                    <div className="height-field">
+                        <p className="field-name">
+                            Height
+                        </p>
+                        <p>
+                            {pkData.height/10 + " m"}
+                        </p>
+                    </div>
+
+                    <div className="weight-field">
+                        <p className="field-name">
+                            Weight
+                        </p>
+                        <p>
+                            {pkData.weight/10 + " kg"}
+                        </p>
+                    </div>
+
+                    <div className="skills-field">
+                        <p className="field-name">
+                            Skills
+                        </p>
+                        <ul>
+                            <PokemonSkillsField pkData={pkData}/>
+                        </ul>
+                    </div>
+
+                    <div className="gender-field">
+                        <p className="field-name">
+                            Gender
+                        </p>
+                        <div className="gender-icons">
+                            <PokemonGenderField pkData={pkData} />
+                        </div>
+                    </div>       
+                </div>
+
+                <div className="type-and-weakness">
+                    <div className="type-field">
+                        <p>Type</p>
+                        <ul>
+                            <PokemonTypeField pkData={pkData}/>
+                        </ul>
+                    </div>
+
+                    <div className="weaknesses-field">
+                        <p>Weaknesses</p>
+                        <ul>
+                            <PokemonWeaknessesField pkData={pkData}/>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
             <div className="evolutios-field">
